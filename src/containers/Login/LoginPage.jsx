023ea5016e71f5cfbd4141723451567cbfa4d53c
ashik112 <md.ashekur@gmail.com>
2020-06-19 20:58:50 +0600

    use formik

--- src/containers/Login/LoginPage.jsx
+++ src/containers/Login/LoginPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Form, Button, Container, Row, Col, Card, Image} from 'react-bootstrap';
+import { Formik, Field } from 'formik';
 import logo from '../../assets/dingi.png';
 import './LoginPage.scss';
 
@@ -15,23 +16,43 @@ const LoginPage = () => (
               <br/>
               <h1 className="text-warning">Dingi</h1>
             </div>
-            <Form>
-              <Form.Group controlId="formBasicEmail">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="text" placeholder="Enter email" />
-                {/*<Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>*/}
-              </Form.Group>
-
-              <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Log In
-              </Button>
-            </Form>
+            <Formik
+              initialValues={{
+                username: '',
+                password: '',
+              }}
+              onSubmit={(values) => {
+                console.log(values);
+              }}
+            >
+              {({ handleSubmit, isSubmitting, getFieldProps, handleChange, handleBlur, values }) => {
+                return (
+                  <Form>
+                    <Field
+                      name="username"
+                      render={({field, formProps}) => (
+                        <Form.Group controlId="username">
+                          <Form.Label>Username</Form.Label>
+                          <Form.Control type="text" placeholder="Enter your username" value={field.value} onChange={field.onChange} />
+                        </Form.Group>
+                      )}
+                    />
+                    <Field
+                      name="password"
+                      render={({field, formProps}) => (
+                        <Form.Group controlId="password">
+                          <Form.Label>Password</Form.Label>
+                          <Form.Control type="password" placeholder="Enter your password" value={field.value} onChange={field.onChange} />
+                        </Form.Group>
+                      )}
+                    />
+                    <Button variant="warning" className="text-light" type="button" onClick={handleSubmit}>
+                      Log In
+                    </Button>
+                  </Form>
+                );
+              }}
+            </Formik>
           </Card.Body>
         </Card>
       </Col>
